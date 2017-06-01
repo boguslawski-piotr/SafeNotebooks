bac18d55a191a1df705ef3759f01688d6c9e8268
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-01 18:57:43 -0400

    Playing with XF bindings system

--- Shared/TestView.xaml.cs
+++ Shared/TestView.xaml.cs
@@ -5,8 +5,36 @@ using Xamarin.Forms;
 
 namespace SafeNotebooks
 {
+    public class TestModel : BindableObject
+    {
+        public static readonly BindableProperty ModelTestPropertyB = BindableProperty.Create("ModelTestProperty", typeof(string), typeof(TestModel), null);
+
+        public static TestModel Current;
+
+        public string ModelTestProperty {
+            get => (string)GetValue(ModelTestPropertyB);
+            set => SetValue(ModelTestPropertyB, value);
+        }
+
+        public TestModel()
+        {
+            ModelTestProperty = "empty constructor";
+            Current = this;
+        }
+
+        public TestModel(string s)
+        {
+            ModelTestProperty = s;
+            Current = this;
+        }
+    }
+
     public partial class TestView : ModalContentView
     {
+        public static TestModel Model { get; } = new TestModel("constructor from TestView");
+
+        public static string TestProperty => "static test property";
+
         public TestView()
         {
             InitializeComponent();
@@ -16,18 +44,18 @@ namespace SafeNotebooks
         {
             //MainWnd.Current.IsSplitView = true;
             //MainWnd.Current.ShowDetailView<PageView>("vPage", MasterDetailPageEx.ViewsSwitchingAnimation.LeftToRight);
-			MainWnd.Current.ShowDetailViewAsync<PageView>(MasterDetailPageEx.ViewsSwitchingAnimation.Back);
-		}
-	
+            MainWnd.Current.ShowDetailViewAsync<PageView>(MasterDetailPageEx.ViewsSwitchingAnimation.Back);
+        }
+
         void MS_Clicked(object sender, System.EventArgs e)
-		{
-            MainWnd.Current.ShowMasterViewAsync<NotebookView>( MasterDetailPageEx.ViewsSwitchingAnimation.Back);
+        {
+            MainWnd.Current.ShowMasterViewAsync<NotebookView>(MasterDetailPageEx.ViewsSwitchingAnimation.Back);
             MainWnd.Current.MasterViewIsVisible = true;
-		}
+        }
 
         void MH_Clicked(object sender, System.EventArgs e)
-		{
-            MainWnd.Current.MasterViewIsVisible = false;
-		}
-	}
+        {
+            Model.ModelTestProperty = "changed...";
+        }
+    }
 }
