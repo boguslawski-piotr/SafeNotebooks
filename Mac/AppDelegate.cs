632dd495acb3b4632bd4d1415abd3d981d14e2c6
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-05-31 22:17:45 -0400

    SafeNotebooks: playing with macOS apis

--- Mac/AppDelegate.cs
+++ Mac/AppDelegate.cs
@@ -5,24 +5,78 @@ using Xamarin.Forms;
 using Xamarin.Forms.Platform.MacOS;
 
 using SafeNotebooks;
+using pbXNet;
+using System.Reflection;
+using CoreGraphics;
+using Newtonsoft.Json;
 
 namespace Mac
 {
-
     [Register("AppDelegate")]
 	public class AppDelegate : FormsApplicationDelegate
 	{
 		NSWindow window;
+
 		public AppDelegate()
 		{
-			var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
-			var rect = new CoreGraphics.CGRect(800, 500, 1024, 768);
-            // TODO: center on screen or restore last (remember: x,y => left-bottom!)
+			var settings = NSUserDefaults.StandardUserDefaults;
+            const string wt = "__wt";
+            const string wl = "__wl";
+            const string ww = "__ww";
+            const string wh = "__wh";
 
-            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            CGPoint topleft = new CGPoint()
+			{
+				Y = settings.IntForKey(wt),
+				X = settings.IntForKey(wl)
+			};
+            topleft.X = topleft.Y = 0;
 
-			window.Title = "Safe Notebooks"; // TODO: translation
-			window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+			CGSize size = new CGSize()
+			{
+				Height = settings.IntForKey(wh),
+				Width = settings.IntForKey(ww)
+			};
+
+            size.Height = size.Height <= 0 ? 600 : size.Height;
+			size.Width = size.Width <= 0 ? 800 : size.Width;
+
+			CGRect screen = NSScreen.MainScreen.Frame;
+            CGRect screenFrame = NSScreen.MainScreen.VisibleFrame;
+
+            //var rect = new CoreGraphics.CGRect(topleft.X <= 0 ? screen.Width / 2 - size.Width / 2 : topleft.X, 
+                                               //topleft.Y <= 0 ? screen.Height / 2 - size.Height / 2 : topleft.Y, 
+                                               //size.Width, 
+                                               //size.Height);
+            var rect = new CoreGraphics.CGRect(topleft.X <= 0 ? 100 : topleft.X,
+                                               topleft.Y <= 0 ? screen.Height - size.Height - (100 + (screen.Height - screenFrame.Height)) : topleft.Y,
+											   size.Width,
+											   size.Height);
+			var style = NSWindowStyle.Miniaturizable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+			window = new NSWindow(rect, style, NSBackingStore.Buffered, false)
+            {
+                Title = "",
+                TitleVisibility = NSWindowTitleVisibility.Hidden,
+				MinSize = new CGSize(800, 600),
+			};
+
+            //
+
+            window.DidMove += (object sender, System.EventArgs e) =>
+            {
+                CGRect c = window.ContentRectFor(window.Frame);
+				CGRect f = window.Frame;
+                settings.SetInt((System.nint)(c.Top + (f.Height - c.Height)), wt);
+				settings.SetInt((System.nint)c.Left, wl);
+            };
+
+            window.DidResize += (object sender, System.EventArgs e) =>
+            {
+                CGRect c = window.ContentRectFor(window.Frame);
+                CGRect f = window.Frame;
+				settings.SetInt((System.nint)c.Width, ww);
+                settings.SetInt((System.nint)(c.Height - (f.Height - c.Height)), wh);
+			};
 		}
 
 		public override NSWindow MainWindow
@@ -36,5 +90,7 @@ namespace Mac
 			LoadApplication(new App());
 			base.DidFinishLaunching(notification);
 		}
+
+        
 	}
 }
