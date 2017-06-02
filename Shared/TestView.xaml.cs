c3310aeaf1bd861b6be144052dbb8a8ae333e16e
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-01 23:07:26 -0400

    SafeNotebooks: some tests...

--- Shared/TestView.xaml.cs
+++ Shared/TestView.xaml.cs
@@ -44,13 +44,13 @@ namespace SafeNotebooks
         {
             //MainWnd.Current.IsSplitView = true;
             //MainWnd.Current.ShowDetailView<PageView>("vPage", MasterDetailPageEx.ViewsSwitchingAnimation.LeftToRight);
-            MainWnd.Current.ShowDetailViewAsync<PageView>(MasterDetailPageEx.ViewsSwitchingAnimation.Back);
+            //MainWnd.Current.ShowDetailViewAsync<PageView>(MasterDetailPageEx.ViewsSwitchingAnimation.Back);
         }
 
         void MS_Clicked(object sender, System.EventArgs e)
         {
-            MainWnd.Current.ShowMasterViewAsync<NotebookView>(MasterDetailPageEx.ViewsSwitchingAnimation.Back);
-            MainWnd.Current.MasterViewIsVisible = true;
+            //MainWnd.Current.ShowMasterViewAsync<NotebookView>(MasterDetailPageEx.ViewsSwitchingAnimation.Back);
+            //MainWnd.Current.MasterViewIsVisible = true;
         }
 
         void MH_Clicked(object sender, System.EventArgs e)
