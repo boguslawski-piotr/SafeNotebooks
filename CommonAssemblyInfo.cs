a131e17433537c7471b039b2c38edf1b1da5a75b
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-16 17:12:24 -0400

    Next: Revision

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Resources;
 
 [assembly: AssemblyTitle("Safe Notebooks")]
 [assembly: AssemblyDescription("")]
@@ -7,11 +8,11 @@ using System.Reflection;
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0.15")]
-[assembly: AssemblyFileVersion("1.0.0.15")]
+[assembly: AssemblyVersion("1.0.0.16")]
+[assembly: AssemblyFileVersion("1.0.0.16")]
